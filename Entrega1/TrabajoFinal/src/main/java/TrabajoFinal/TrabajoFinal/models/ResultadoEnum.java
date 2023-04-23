4243a17754cbc8d7fb3abad1f99c36397ba02f06
EduardoP312 <127903574+EduardoP312@users.noreply.github.com>
2023-04-23 05:37:12 -0300

    Add files via upload

---
+++ Entrega1/TrabajoFinal/src/main/java/TrabajoFinal/TrabajoFinal/models/ResultadoEnum.java
@@ -0,0 +1,8 @@
+package TrabajoFinal.TrabajoFinal.models;
+
+public enum ResultadoEnum {
+    GANADOR,
+    PERDEDOR,                  //Metodo para obtener uno de los 3 resultados escritos.
+    EMPATE
+}
+
