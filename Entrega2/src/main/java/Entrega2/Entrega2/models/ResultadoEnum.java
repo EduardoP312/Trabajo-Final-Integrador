1f67f681fa3fe56037704e4e78af7d83b52800d0
EduardoP312 <127903574+EduardoP312@users.noreply.github.com>
2023-04-27 05:55:30 -0300

    Subiendo Entrega2, Codigo Corregido

---
+++ Entrega2/src/main/java/Entrega2/Entrega2/models/ResultadoEnum.java
@@ -0,0 +1,8 @@
+package Entrega2.Entrega2.models;
+
+public enum ResultadoEnum {
+    GANADOR,
+    PERDEDOR,                  //Metodo para obtener uno de los 3 resultados escritos.
+    EMPATE
+}
+
