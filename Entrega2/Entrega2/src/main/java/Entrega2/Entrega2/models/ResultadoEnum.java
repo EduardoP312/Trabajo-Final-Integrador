0a34ce33f6e66b04b22d5c2f0ff6c65a8eb373d6
EduardoP312 <127903574+EduardoP312@users.noreply.github.com>
2023-04-27 05:52:59 -0300

    Delete Entrega2/Entrega2 directory

--- Entrega2/Entrega2/src/main/java/Entrega2/Entrega2/models/ResultadoEnum.java
+++
@@ -1,8 +0,0 @@
-package Entrega2.Entrega2.models;
-
-public enum ResultadoEnum {
-    GANADOR,
-    PERDEDOR,                  //Metodo para obtener uno de los 3 resultados escritos.
-    EMPATE
-}
-
