07242086217fa5dc7ce5ba6fa3a3530a24a5a18e
EduardoP312 <127903574+EduardoP312@users.noreply.github.com>
2023-04-27 05:52:38 -0300

    Delete Entrega1/TrabajoFinal directory

--- Entrega1/TrabajoFinal/src/main/java/TrabajoFinal/TrabajoFinal/models/ResultadoEnum.java
+++
@@ -1,8 +0,0 @@
-package TrabajoFinal.TrabajoFinal.models;
-
-public enum ResultadoEnum {
-    GANADOR,
-    PERDEDOR,                  //Metodo para obtener uno de los 3 resultados escritos.
-    EMPATE
-}
-
